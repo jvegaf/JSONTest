db48366f314f6fcfd46ff58b9b78537ef893b2b3
JOSE VEGA <jose@MacBook-Pro-de-Javier.local>
2013-09-06 00:45:09 +0200

    Initial Commit

---
+++ JSONTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JSONTest
+//
+//  Created by JOSE VEGA on 06/09/13.
+//  Copyright (c) 2013 JOSE VEGA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
